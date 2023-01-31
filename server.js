51726ec0401f786f18311d487cf25e3902f4d2a9
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-30 19:29:36 -0500

    modulizing routes for /api

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express");
-
+const api = require(`./routes/index.js`)
 const app = express()
 
 app.use(express.static("./public/"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+app.use('api', api)
 const PORT = process.env.PORT || 3001;
 
+// Route for a GET request that will return homepage
 app.get(`/`, (req,res) => {
-
-} )
-
+    res.status(200).sendFile(`${__dirname}/public`);
+})
 
 app.listen(PORT, () =>{
     console.log(`Serving static asset routes at http://localhost:${PORT}`)
