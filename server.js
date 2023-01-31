a11d6baf6aee372d08577100303e2478d616ab7a
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-30 19:59:17 -0500

    Route for a GET request that will return homepage (index.html)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 // const api = require(`./routes/index.js`)
+const notes = require(`./db/db.json`)
 const app = express()
 
 // Middleware for parsing JSON and urlencoded form data
@@ -11,9 +12,18 @@ app.use(express.static("./public/"));
 
 const PORT = process.env.PORT || 3001;
 
-// Route for a GET request that will return homepage
+// Route for a GET request that will return notes.html
 app.get(`/notes/`, (req,res) => {
     res.status(200).sendFile(`${__dirname}/public/notes.html`);
+})
+
+// Route for a GET request that will return homepage (index.html)
+app.get('*', (req, res) => {
+    res.status(200).sendFile(`${__dirname}/public/index.html`)
+})
+// Route for a GET request that will return JSON
+app.get(`/api/notes/`, (req,res) => {
+    res.status(200).json(notes);
 })
 
 
