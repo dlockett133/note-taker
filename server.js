eca6f6ff3391f4e15bbe656bbd1470c3375f362c
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-30 19:46:29 -0500

    Route for a GET request that will return notes.html

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const api = require(`./routes/index.js`)
+// const api = require(`./routes/index.js`)
 const app = express()
 
-app.use(express.static("./public/"));
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+// app.use('api', api)
 
-app.use('api', api)
+app.use(express.static("./public/"));
+
 const PORT = process.env.PORT || 3001;
 
 // Route for a GET request that will return homepage
-app.get(`/`, (req,res) => {
-    res.status(200).sendFile(`${__dirname}/public`);
+app.get(`/notes/`, (req,res) => {
+    res.status(200).sendFile(`${__dirname}/public/notes.html`);
 })
+
 
 app.listen(PORT, () =>{
     console.log(`Serving static asset routes at http://localhost:${PORT}`)
