409d3356d13dbeb434c07bbad6615f4ad7d79bc9
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-30 19:20:14 -0500

    initialized Port

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 const PORT = process.env.PORT || 3001;
+
+app.get(`/`, (req,res) => {
+
+} )
+
+
+app.listen(PORT, () =>{
+    console.log(`Serving static asset routes at http://localhost:${PORT}`)
+})
