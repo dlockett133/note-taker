8994921c29de4e943adeca62de6e63a683352ff6
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-30 21:13:18 -0500

    parsed JSON file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs")
 // const api = require(`./routes/index.js`)
 const notes = require(`./db/db.json`);
 const uuid = require("./helpers/uuid");
@@ -18,10 +19,6 @@ app.get(`/notes/`, (req,res) => {
     res.status(200).sendFile(`${__dirname}/public/notes.html`);
 })
 
-// Route for a GET request that will return homepage (index.html)
-app.get('*', (req, res) => {
-    res.status(200).sendFile(`${__dirname}/public/index.html`)
-})
 // Route for a GET request that will return JSON
 app.get(`/api/notes/`, (req,res) => {
     res.status(200).json(notes);
@@ -29,8 +26,6 @@ app.get(`/api/notes/`, (req,res) => {
 
 app.post(`/api/notes/`, (req, res) => {
     console.log(`${req.method} is now logged =)`)
-    console.log(`${req.body} is now logged =)`)
-
     const {title, text} =  req.body;
 
     if (title && text) {
@@ -44,11 +39,26 @@ app.post(`/api/notes/`, (req, res) => {
             body: newNote
         }
     
+    fs.readFile("./db/db.json", `utf-8`, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            const parsedNotes = JSON.parse(data)
+
+        }
+    })
         res.status(201).json(response)
+        console.log(`${response} is now logged =)`)
+
     } else {
         res.status(500).json(`error in posting note`)
     }
 
+})
+
+// Route for a GET request that will return homepage (index.html)
+app.get('*', (req, res) => {
+    res.status(200).sendFile(`${__dirname}/public/index.html`)
 })
 
 app.listen(PORT, () =>{
