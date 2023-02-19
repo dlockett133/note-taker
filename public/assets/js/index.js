41c43047c3826c65d9b52ab6738ffc97cf292254
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-19 03:23:03 -0500

    removed debugger

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -51,7 +51,6 @@ const deleteNote = (id) =>
   });
 
 const renderActiveNote = () => {
-  debugger
   hide(saveNoteBtn);
 
   if (activeNote.id) {
@@ -98,7 +97,6 @@ const handleNoteDelete = (e) => {
 
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
-  debugger
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
